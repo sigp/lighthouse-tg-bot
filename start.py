8c1509539fa3c736fd3cb519aef70be6a6baa075
Paul Hauner <paul@paulhauner.com>
2021-07-21 19:05:24 +1000

    Tidy

--- start.py
+++ start.py
@@ -17,8 +17,12 @@ bot.
 
 import logging
 import os
+import requests
+import json
+import yaml
+import sys
 
-from telegram import Update, Message
+from telegram import Update, Message, ParseMode
 from telegram.ext import (
     Updater,
     CommandHandler,
@@ -33,6 +37,14 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+SLOTS_PER_EPOCH = 32
+POLL_TIME = 30
+EPOCH_LOOKBACK=2
+
+# Testing variables.
+EVERYTHING_OK_ALARM=False  # Set to False for production.
+ONCE_PER_EPOCH=True    # Set to True for production.
+
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -45,17 +57,71 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hi! Use /subscribe <password> to get notifications')
 
 
-def update(context: CallbackContext) -> None:
+def get_current_epoch(bn_api: str) -> int:
+    r = requests.get(bn_api + "/eth/v1/beacon/headers/head");
+    slot = int(r.json()['data']['header']['message']['slot'])
+    return slot // SLOTS_PER_EPOCH
+
+def message_with_json(message, json_data) -> str:
+    return "{}\n\n```json\n{}\n```".format(
+        message,
+        json.dumps(json_data, indent=1)
+    )
+
+def process_performance_data(bn_api, epoch, context: CallbackContext) -> [str]:
+    if epoch < EPOCH_LOOKBACK:
+        return
+    else:
+        epoch -= EPOCH_LOOKBACK
+
+    data = context.bot_data["validator_indices"]
+    url = "{}/lighthouse/attestation_performance/{}".format(bn_api, epoch)
+    print(url)
+    r = requests.post(url, json=data);
+    response = r.json()
+    messages = []
+    for v in response:
+        index = v["validator_index"]
+        if not v["is_optimal"]:
+            messages.append(message_with_json(
+                "🤒 Validator {} suboptimal in epoch {} 🤒".format(index, epoch),
+                v
+            ))
+        else:
+            if EVERYTHING_OK_ALARM:
+                messages.append(message_with_json(
+                    "🚀 Validator {} optimal in epoch {} 🚀".format(index, epoch),
+                    v
+                ))
+    return messages
+
+
+def poll_epoch(context: CallbackContext) -> None:
     chat_ids = context.bot_data['chat_ids']
-    for chat_id in chat_ids:
-        context.bot.send_message(chat_id, "ping")
+    bn_api = context.bot_data['bn_api']
+    try:
+        current_epoch = get_current_epoch(bn_api)
+        if current_epoch > context.bot_data['head_epoch']:
+            if ONCE_PER_EPOCH == True:
+                context.bot_data['head_epoch'] = current_epoch
+            messages = process_performance_data(bn_api, current_epoch, context)
+            for message in messages:
+                for chat_id in chat_ids:
+                    context.bot.send_message(chat_id, message, parse_mode=ParseMode.MARKDOWN_V2)
+    except Exception as e:
+        for chat_id in chat_ids:
+            context.bot.send_message(
+                chat_id,
+                "Error polling epoch {}: {}".format(current_epoch, e)
+            )
+        raise e
 
 
 def subscribe(update: Update, context: CallbackContext) -> None:
     try:
-        password = os.environ['BOT_PASSWORD']
+        password = context.bot_data['password']
         if password == "":
-            update.message.reply_text("Refusing to subscribe without BOT_PASSWORD")
+            update.message.reply_text("Refusing to subscribe without bot_password")
             return
 
         supplied_password = str(context.args[0])
@@ -83,9 +149,16 @@ def unsubscribe(update: Update, context: CallbackContext) -> None:
 
 
 def main() -> None:
+    config_path = sys.argv[1]
+    with open(config_path, 'r') as stream:
+        config = yaml.safe_load(stream)
+        token = config['telegram_token']
+        bn_api = config['bn_api']
+        password = config['bot_password']
+        validator_indices = config['validator_indices']
+
     """Run bot."""
     persistence = PicklePersistence(filename='bot-db')
-    token = os.environ['TG_TOKEN']
     # Create the Updater and pass it your bot's token.
     updater = Updater(token, persistence=persistence)
 
@@ -94,6 +167,11 @@ def main() -> None:
 
     if "chat_ids" not in dispatcher.bot_data.keys():
         dispatcher.bot_data['chat_ids'] = set()
+    if "head_epoch" not in dispatcher.bot_data.keys():
+        dispatcher.bot_data['head_epoch'] = 0
+    dispatcher.bot_data['bn_api'] = os.environ['BN_API']
+    dispatcher.bot_data['password'] = password
+    dispatcher.bot_data['validator_indices'] = validator_indices
 
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
@@ -102,7 +180,7 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("unsubscribe", unsubscribe))
 
     # Start a task to poll the beacon node
-    updater.job_queue.run_repeating(update, 10)
+    updater.job_queue.run_repeating(poll_epoch, 30)
 
     # Start the Bot
     updater.start_polling()
