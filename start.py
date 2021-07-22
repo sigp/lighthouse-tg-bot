77336025dd8411d9513fbf02c4702c70caafbefc
Paul Hauner <paul@paulhauner.com>
2021-07-22 11:21:27 +1000

    Tidy

--- start.py
+++ start.py
@@ -37,6 +37,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+# Configuration constants.
 SLOTS_PER_EPOCH = 32
 POLL_TIME = 30
 EPOCH_LOOKBACK=2
@@ -57,58 +58,91 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hi! Use /subscribe <password> to get notifications')
 
 
+# Request the current epoch from the beacon node.
 def get_current_epoch(bn_api: str) -> int:
     r = requests.get(bn_api + "/eth/v1/beacon/headers/head");
     slot = int(r.json()['data']['header']['message']['slot'])
     return slot // SLOTS_PER_EPOCH
 
+# Format a message with a text header and JSON footer.
 def message_with_json(message, json_data) -> str:
     return "{}\n\n```json\n{}\n```".format(
         message,
         json.dumps(json_data, indent=1)
     )
 
-def process_performance_data(bn_api, epoch, context: CallbackContext) -> [str]:
-    if epoch < EPOCH_LOOKBACK:
-        return
-    else:
-        epoch -= EPOCH_LOOKBACK
-
-    data = context.bot_data["validator_indices"]
+# Request attestation performance data from the beacon node.
+def get_peformance_data(bn_api, current_epoch, indices):
     url = "{}/lighthouse/attestation_performance/{}".format(bn_api, epoch)
-    print(url)
-    r = requests.post(url, json=data);
-    response = r.json()
+    r = requests.post(url, json=indices)
+    return r.json()
+
+# Translate perfomance data for a single validator into a list of messages.
+def process_validator_performance_data(v, epoch) -> [str]:
     messages = []
-    for v in response:
-        index = v["validator_index"]
-        if not v["is_optimal"]:
+
+    index = v["validator_index"]
+    if not v["is_optimal"]:
+        messages.append(message_with_json(
+            "🤒 Validator {} suboptimal in epoch {} 🤒".format(index, epoch),
+            v
+        ))
+    else:
+        if EVERYTHING_OK_ALARM:
             messages.append(message_with_json(
-                "🤒 Validator {} suboptimal in epoch {} 🤒".format(index, epoch),
+                "🚀 Validator {} optimal in epoch {} 🚀".format(index, epoch),
                 v
             ))
-        else:
-            if EVERYTHING_OK_ALARM:
-                messages.append(message_with_json(
-                    "🚀 Validator {} optimal in epoch {} 🚀".format(index, epoch),
-                    v
-                ))
+
     return messages
 
+# Translate data from the beacon node into a list of messages.
+def process_performance_data(performance_data, epoch, context: CallbackContext) -> [str]:
+    messages = []
 
-def poll_epoch(context: CallbackContext) -> None:
+    for v in performance_data:
+        try:
+            messages += process_validator_performance_data(v, epoch)
+        except Exception as e:
+            index = v["validator_index"]
+            messages.append(
+                "Error processing validator {}: {}".format(index, e)
+            )
+
+    return messages
+
+# Poll the beacon node for the current epoch and determine if an update is
+# required.
+def poll_performance_data(context: CallbackContext) -> None:
     chat_ids = context.bot_data['chat_ids']
     bn_api = context.bot_data['bn_api']
+    indices = context.bot_data["validator_indices"]
     try:
+        # Request the current epoch from the beacon node.
         current_epoch = get_current_epoch(bn_api)
+
         if current_epoch > context.bot_data['head_epoch']:
+            # If enabled, de-bounce so we query once per epoch.
             if ONCE_PER_EPOCH == True:
                 context.bot_data['head_epoch'] = current_epoch
-            messages = process_performance_data(bn_api, current_epoch, context)
+
+            # Always request data for a prior epoch.
+            if current_epoch < EPOCH_LOOKBACK:
+                return
+            else:
+                request_epoch = current_epoch - EPOCH_LOOKBACK
+
+            # Request data from the beacon node.
+            performance_data = get_performance_data(bn_api, request_epoch, indices)
+            # Translate beacon node data into messages.
+            messages = process_performance_data(performance_data, request_epoch, context)
+
+            # Send messages out to all subscribers.
             for message in messages:
                 for chat_id in chat_ids:
                     context.bot.send_message(chat_id, message, parse_mode=ParseMode.MARKDOWN_V2)
     except Exception as e:
+        # Update failed, send an error message to all subscribers.
         for chat_id in chat_ids:
             context.bot.send_message(
                 chat_id,
@@ -117,27 +151,29 @@ def poll_epoch(context: CallbackContext) -> None:
         raise e
 
 
+# Allow a user to receive updates.
 def subscribe(update: Update, context: CallbackContext) -> None:
     try:
         password = context.bot_data['password']
+        # Do not allow an empty password.
         if password == "":
             update.message.reply_text("Refusing to subscribe without bot_password")
             return
 
+        # Validate password.
         supplied_password = str(context.args[0])
         if supplied_password != password:
             update.message.reply_text("Invalid password")
             return
 
-        if "chat_ids" not in context.bot_data.keys():
-            context.bot_data['chat_ids'] = set()
+        # Subscribe chat id, regardless of an existing subscription.
         context.bot_data['chat_ids'].add(update.message.chat_id)
-
         update.message.reply_text("You are subscribed")
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /subscribe <password>')
 
 
+# Allow a user to stop receiving updates.
 def unsubscribe(update: Update, context: CallbackContext) -> None:
     chat_ids = context.bot_data['chat_ids']
     chat_id = update.message.chat_id
@@ -179,8 +215,8 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("subscribe", subscribe))
     dispatcher.add_handler(CommandHandler("unsubscribe", unsubscribe))
 
-    # Start a task to poll the beacon node
-    updater.job_queue.run_repeating(poll_epoch, 30)
+    # Start a task to poll the beacon node for performance data.
+    updater.job_queue.run_repeating(poll_performance_data, 30)
 
     # Start the Bot
     updater.start_polling()
